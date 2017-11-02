47bc15a30fccf863443051104ce1954841323104
bojedemant <bd@teas.dk>
2017-11-02 13:45:16 +0100

    04 opg 4

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var k1 = new Person();
+            var k2 = new Person();
+            k1.navn = "a";
+            k2.navn = "b";
+            Console.WriteLine(k1.navn);
+            Console.WriteLine(k2.navn);
+
+            k1 = k2;
+            Console.WriteLine(k1.navn);
+            Console.WriteLine(k2.navn);
+
+            k1.navn = "c";
+            Console.WriteLine(k1.navn);
+            Console.WriteLine(k2.navn);
+
+
+
+        }
+    }
+}
+
+class Person
+{
+    public string navn;
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Boje";
+            string s2 = "Demant";
+            string samletNavn = s1 + " " + s2;
+            string navnStort = s1.ToUpper() + " " + s2.ToUpper();
+            string navnLille = s1.ToLower() + " " + s2.ToLower();
+            string del = samletNavn.Substring(5, 6);
+            
+            Console.WriteLine(samletNavn);
+            Console.WriteLine(navnStort);
+            Console.WriteLine(navnLille);
+            Console.WriteLine(del);
+            string[] navne = samletNavn.Split(' ');
+            for (int i = 0; i < navne.Length; i++)
+                Console.WriteLine(navne[i]);
+            string filePath = @"x:demo.txt";
+            System.IO.File.AppendAllText(filePath, samletNavn);
+        }
+    }
+}
