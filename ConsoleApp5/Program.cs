0780e3e0a4603d80394eb027aeb525cced2a5c63
bojedemant <bd@teas.dk>
2017-11-02 14:55:50 +0100

    04 øvelse 5

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a < 11; a++)
+            {
+                for (int i = 1; i < 11; i++)
+                {
+                    int b = i * a;
+
+                    if (b >= 50) //styr farve alt efter resultat.
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                    else
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                    if (b < 10)
+                         Console.Write("   " + b);
+                  
+                    else
+                    {
+                        if (i == 10)
+                        {
+
+                            
+                                if (b < 100)
+                                    Console.WriteLine("  " + b);
+                                else
+                                    Console.WriteLine(" " + b);
+
+                            
+                        }
+                        else
+                            Console.Write("  " + b);
+                    }
+                }
+
+            }
+        }
+    }
+}
