404316a9894d8369ea16757e45db3189f0b01198
Boje Demant <bd@teas.dk>
2017-11-05 19:37:26 +0100

    05 opgave 1

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = LægSammen(5, 2);
+            Console.WriteLine(res);
+
+            double res2 = BeregnAreal(5);
+            Console.WriteLine(res2);
+
+
+        }
+
+        static int LægSammen(int a,int b)
+        {
+            return a + b;
+        }
+
+        static double BeregnAreal(int radius)
+        {
+
+        }
+
+        static udskriv(string text)
+        {
+
+        }
+
+        static double BeregnMoms(double beløb, double momsPct)
+        {
+
+        }
+        
+        double Gennemsnit{int[] månedsløn){
+        }
+    }
+
+}
