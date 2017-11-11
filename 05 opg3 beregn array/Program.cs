ae83db7000275616f239a2a49d138ffbcfeff28b
Boje Demant <bd@teas.dk>
2017-11-11 14:16:22 +0100

    05 opg3 beregn array

---
+++ 05 opg3 beregn array/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_opg3_beregn_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = { 10, 7, 6, 3, 1, 50, 3 };
+            var res = BeregnOgSorterArray(test);
+
+        }
+
+        public static ArrayResultat BeregnOgSorterArray(int[] a)
+        {
+            ArrayResultat r; //opret pointer til array
+            r.arraySum = 0;
+
+            foreach ( var item in a)
+            {
+                r.arraySum += item;
+            }
+            r.arrayGennemsnit = r.arraySum / a.Length;
+            Array.Sort(a);
+            return r;
+        }
+
+        struct ArrayResultat
+        {
+            public double arraySum;
+            public double arrayGennemsnit;
+        }
+    }
+}
