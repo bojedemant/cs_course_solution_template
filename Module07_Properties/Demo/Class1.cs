6702edeecaa8ede7e7bf4dc3df77c10c847045ba
Bojedemant <bd@teas.dk>
2017-11-14 10:41:03 +0100

    modul 07 opg 5

---
+++ Module07_Properties/Demo/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module07_Properties.Demo
+{
+    public class Person
+    {
+        private string navn;
+
+        public string Navn
+        {
+            get { return navn;}
+            set { navn = value; }
+        }
+
+        public int Alder { get; set; }
+
+        public Person()
+        {
+            this.Navn = "";
+        }
+
+        public Person( string navn,int alder)
+        {
+            this.navn
+            this.lder=
+        }
+    }
+}
---
+++ modul 07  opg 5 static method/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul_07__opg_5_static_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ArealBeregninger.BeregnArealFirkant(10, 10));
+            Console.WriteLine(ArealBeregninger.BeregnArealCirkel(10));
+
+
+
+        }
+    }
+}
+
+
+class ArealBeregninger
+{
+    public static double  BeregnArealFirkant(double højde, double bredde)
+    {
+        return højde * bredde;
+    }
+
+    public static double BeregnArealCirkel(double radius)
+    {
+        return radius * radius * Math.PI;
+    }
+}
