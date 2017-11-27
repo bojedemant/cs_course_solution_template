a78068d54c638a2acc2b20466565f04d26ed6451
boje <bd@teas.dk>
2017-11-27 12:34:07 +0100

    sdfdf

---
+++ Modul10 opg 2 compare/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul10_opg_2_compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hund[] hunde = new Hund[2];
+            hunde[0] = new Hund() { Alder = 10, Navn = "Bulder" };
+            hunde[1] = new Hund() { Alder = 5, Navn = "Lady" };
+            Array.Sort(hunde);
+            foreach (var item in hunde)
+            {
+                Console.WriteLine(item.Navn);
+
+            }
+        }
+    }
+
+    class Hund : ICompareable
+    {
+        public string Navn { get; set; }
+            
+        public int Alder { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Hund hund = obj as Hund; ???
+            if (this Hund < hund.Hund)
+        }
+    }
+
+}
