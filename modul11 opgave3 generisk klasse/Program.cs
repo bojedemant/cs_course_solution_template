8c20781d843858b071fefc71d0c480ce290625a3
boje <bd@teas.dk>
2017-11-27 15:18:57 +0100

    Modul 11 opgave3

---
+++ modul11 opgave3 generisk klasse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul11_opgave3_generisk_klasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Person
+    {
+        public string Fornavn;
+        public string Efternavn;
+    }
+    class Instruktør : Person
+    {
+
+    }
+
+    class Elev : Person
+    {
+
+    }
+}
