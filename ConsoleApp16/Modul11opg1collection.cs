7b652ae96569ad6962a987727535990744b1654e
boje <bd@teas.dk>
2017-11-27 14:58:56 +0100

    modul 11 opgave2

---
+++ ConsoleApp16/Modul11opg1collection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> personer = new List<Person>();
+            personer.Add(new Person() { Id = 1, Navn = "Navn1"});
+            personer.Add(new Person() { Id = 2, Navn = "Navn2"});
+            personer.Add(new Person() { Id = 3, Navn = "Navn3"});
+            foreach (var item in personer)
+            {
+                Console.WriteLine(item.Navn);
+            }
+
+            Dictionary<int, Person> personer2 = new Dictionary<int, Person>();
+            personer2.Add(1, new Person() { Id = 1, Navn = "Navn4" });
+            personer2.Add(2, new Person() { Id = 1, Navn = "Navn5" });
+            personer2.Add(3, new Person() { Id = 1, Navn = "Navn6" });
+            var p = personer2[2];
+            Console.WriteLine(p.Navn);
+            
+            
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string Navn  { get; set; }
+    }
+}
---
+++ ConsoleApp17/modul11opgave2collection.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class modul11opgave2collection
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Kort
+    {
+
+        public string Kulør { get; set; }
+        public int Værdi { set; get; }
+
+        public override string ToString()
+        {
+            return this.Kulør + "" + this.Værdi;
+        }
+    }
+
+    public class Bunke
+    {
+        private Stack<Kort> bunke = new Stack<Kort>();
+
+
+        public void TilføjKort(Kort K)
+        {
+            bunke.Push(K);
+        }
+
+        public FjernKort(void)
+        {
+            if (bunke.Count>0)
+                return bunke.Pop();
+            return null;
+        }
+
+        public void vis()
+        {
+            foreach (var item in bunke)
+            {
+
+            }
+        }
+    }
+}
