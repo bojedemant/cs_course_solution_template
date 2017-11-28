7fcbfa554fbf4928391808ce9c78213dae994830
boje <bd@teas.dk>
2017-11-28 11:06:19 +0100

    modul 12 delegates opg 1

---
+++ Modul12 opg1 Delegates/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul12_opg1_Delegates
+{
+    class Program
+    {
+        public delegate int BeregnDelegate(int a, int b);  //oprettelse af delegate (mulighed for at kalde ekstern funktion)
+        static void Main(string[] args)
+        {
+            int res = Beregner(1, 2, Plus);
+            res = Beregner(4, 2, Minus);
+            res = Beregner(2, 2, Gange);
+            res = Beregner(10, 2, Divider);
+         //   res = Beregner(10, 2, Sivider);
+        }
+
+        
+
+        public static int Beregner(int a, int b, BeregnDelegate funktion)   // opret funktion som kalder fuktioner af den type der er beskrevet i delegatereklæringen. funktionsnavnet overføres ved kald og compiler finde funktion i liste af muligheder
+        {
+            return funktion(a, b);
+        }
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Divider(int a, int b)
+        {
+            return a / b;
+        }
+        public static int Gange(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+}
